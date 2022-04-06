2bbfecb28e222ca43271c92a116675a89f97298f
turleo <34786670+turleo@users.noreply.github.com>
2022-04-06 14:57:37 +1000

    теперь джойстик работают вместе с поворотом камеры

--- Assets/Scripts/Move/MouseLook.cs
+++ Assets/Scripts/Move/MouseLook.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Move
@@ -9,25 +10,33 @@ namespace Move
 
         public Transform playerBody;
 
+        public Joystick joystick;
+
         float _xRotation = 0f;
 
         // Start is called before the first frame update
         void Start()
         {
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBPLAYER
             Cursor.lockState = CursorLockMode.Locked;
+#endif
         }
 
         // Update is called once per frame
         void Update()
         {
-            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            if (joystick.Direction == Vector2.zero)
+            {
+                float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+                float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-            _xRotation -= mouseY;
-            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+                _xRotation -= mouseY;
+                _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
 
-            transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
-            playerBody.Rotate(Vector3.up * mouseX);
+                transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+                playerBody.Rotate(Vector3.up * mouseX);
+            }
+            Debug.Log(joystick.Direction);
         }
     }
 }
