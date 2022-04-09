54c7e69fda7b8199a9ce31acee3517bc1f117c6a
turleo <34786670+turleo@users.noreply.github.com>
2022-04-09 16:08:16 +1000

    mian menu added

---
+++ Assets/Scripts/MainMenu/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour
+{
+    public void NextLevel(string _scenename)
+    {
+        SceneManager.LoadScene(_scenename);
+    }
+}
